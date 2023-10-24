31bba8c55ddc3621d1edbda4d19e782c6708bea6
ragnamarie <ragna.steinboemer@gmail.com>
2023-10-24 11:17:20 +0200

    add component

--- react-basics_attributes/src/App.js
+++ react-basics_attributes/src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <Article>Replace me with an article</Article>;
+}
+
+export function Article() {
+  return (
+    <article>
+      <h2 className="article__title">random features</h2>
+      <label htmlFor="type">type something</label>
+      <input type="text" id="type" name="type"></input>
+      <a
+        className="article__link"
+        href="https://en.wikipedia.org/wiki/Succession_(TV_series)"
+      ></a>
+    </article>
+  );
 }
