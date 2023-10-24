21576a3ab39e1a679072cd4ca37a75f691beeb0b
ragnamarie <ragna.steinboemer@gmail.com>
2023-10-24 10:57:13 +0200

    add component

--- react-basics_hello-world-article/src/App.js
+++ react-basics_hello-world-article/src/App.js
@@ -2,5 +2,15 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return <HelloWorldArticle>Replace me with your Component!</HelloWorldArticle>;
+}
+
+export function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>
+        <p>bonjour!</p>
+      </h1>
+    </article>
+  );
 }
